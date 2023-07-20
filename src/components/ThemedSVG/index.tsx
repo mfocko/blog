62e0bfeb85a2b156140958e3114556b6c3b3d469
Matej Focko <mfocko@redhat.com>
2023-07-20 20:53:14 +0200

    fix: make themed SVG fit the width
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- src/components/ThemedSVG/index.tsx
+++ src/components/ThemedSVG/index.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 import clsx from 'clsx';
 
+import styles from './styles.module.css';
+
 export default function ThemedSVG(props): JSX.Element {
     const { source, className: parentClassName, alt, ...propsRest } = props;
 
     return (
         <>
             <embed
-                className={clsx("light-mode-only", parentClassName)}
+                className={clsx("light-mode-only", parentClassName, styles.themed_svg)}
                 type="image/svg+xml"
                 src={`${source}_light.svg`}
             />
             <embed
-                className={clsx("dark-mode-only", parentClassName)}
+                className={clsx("dark-mode-only", parentClassName, styles.themed_svg)}
                 type="image/svg+xml"
                 src={`${source}_dark.svg`}
             />
