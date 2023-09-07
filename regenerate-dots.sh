326612b3e2602fef73f263480d468e82c460606c
Matej Focko <mfocko@redhat.com>
2023-09-07 17:59:11 +0200

    revert: "fix: switch graphviz to pngs as it is unusable now"
    This reverts commit a5b6f8b1286f8738c10ad7f149a255b47eb11a05.

--- regenerate-dots.sh
+++ regenerate-dots.sh
@@ -3,11 +3,11 @@
 FONT="'Cascadia Code PL', 'JetBrains Mono', 'Iosevka', 'Fira Code', 'Hack', monospace"
 
 for pic in $(find ./static/files -name '*.dot' -print); do
-    PNG_NAME=".$(echo $pic | cut -d'.' -f2)"
+    SVG_NAME=".$(echo $pic | cut -d'.' -f2)"
 
     # light mode
-    dot $pic -Tpng -Gfontname="$FONT" -Nfontname="$FONT" -Efontname="$FONT" > ${PNG_NAME}_light.png
+    dot $pic -Tsvg -Gfontname="$FONT" -Nfontname="$FONT" -Efontname="$FONT" > ${SVG_NAME}_light.svg
 
     # dark mode
-    dot $pic -Tpng -Gfontname="$FONT" -Nfontname="$FONT" -Efontname="$FONT" -Gbgcolor="#1b1b1d" -Gcolor="white" -Gfontcolor="white" -Nfillcolor="none" -Ncolor="white" -Nfontcolor="white" -Efillcolor="none" -Ecolor="white" -Efontcolor="white" > ${PNG_NAME}_dark.png
+    dot $pic -Tsvg -Gfontname="$FONT" -Nfontname="$FONT" -Efontname="$FONT" -Gbgcolor="#1b1b1d" -Gcolor="white" -Gfontcolor="white" -Nfillcolor="none" -Ncolor="white" -Nfontcolor="white" -Efillcolor="none" -Ecolor="white" -Efontcolor="white" > ${SVG_NAME}_dark.svg
 done;
