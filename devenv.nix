244f4b4b4123a3e9624773912339f3eb057bf737
Matej Focko <me@mfocko.xyz>
2025-07-12 15:18:39 +0200

    chore(devenv): convert from nix-shell to devenv
    Fixes #31
    
    Signed-off-by: Matej Focko <me@mfocko.xyz>

---
+++ devenv.nix
@@ -0,0 +1,31 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [
+    # To be able to regenerate the graphs
+    pkgs.graphviz-nox
+  ];
+
+  languages.javascript = {
+    enable = true;
+    yarn = {
+      enable = true;
+      install.enable = true;
+    };
+  };
+
+  processes.serve.exec = "URL=http://localhost BASE_URL=/ yarn run start --no-open";
+
+  tasks = {
+    "assets:archives".exec = "sh regenerate-archives.sh";
+    "assets:dots".exec = "sh regenerate-dots.sh";
+
+    "blog:build" = {
+      exec = "yarn run build";
+      before = [
+        "assets:dots"
+        "assets:archives"
+      ];
+    };
+  };
+}
