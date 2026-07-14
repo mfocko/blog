c4b9efe4545da147cf49f36a2b5e13e472f2232c
Matej Focko <me@mfocko.xyz>
2026-07-14 14:06:37 +0200

    ci(devenv): move scripts for assets into devenv
    Signed-off-by: Matej Focko <me@mfocko.xyz>

--- devenv.nix
+++ devenv.nix
@@ -24,8 +24,50 @@
   processes.serve.exec = "URL=http://localhost BASE_URL=/ yarn run start --no-open";
 
   tasks = {
-    "assets:archives".exec = "sh regenerate-archives.sh";
-    "assets:dots".exec = "sh regenerate-dots.sh";
+    "assets:archives".exec = ''
+      # remove preexisting archives
+      echo "[INFO] Removing pre-existing archives"
+      find ./static/files -name '*.tar.gz' -exec rm {} \;
+      find ./static/files -name '*.tar.bz2' -exec rm {} \;
+
+      ROOT_DIR=$PWD
+
+      for relative_path in $(find ./static/files -name '.archive' -print); do
+          echo;
+
+          relative_path=$(dirname $relative_path)
+          base=$(basename $relative_path)
+          cd $relative_path/..
+
+          all_files=$(find $base/** ! -name '.archive' -print)
+
+          echo "[INFO] Compressing $base.tar.gz"
+          tar caf $base.tar.gz $all_files
+
+          echo "[INFO] Compressing $base.tar.bz2"
+          tar caf $base.tar.bz2 $all_files
+
+          cd $ROOT_DIR
+      done;
+    '';
+    "assets:dots".exec = ''
+      FONT="'Cascadia Code PL', 'JetBrains Mono', 'Iosevka', 'Fira Code', 'Hack', monospace"
+
+      for pic in $(find ./static/files -name '*.dot' -print); do
+          SVG_NAME=".$(echo $pic | cut -d'.' -f2)"
+
+          # light mode
+          dot $pic -Tsvg \
+            -Gfontname="$FONT" -Nfontname="$FONT" -Efontname="$FONT" > ''${SVG_NAME}_light.svg
+
+          # dark mode
+          dot $pic -Tsvg \
+            -Gfontname="$FONT" -Nfontname="$FONT" -Efontname="$FONT" \
+            -Gbgcolor="#1b1b1d" -Gcolor="white" -Gfontcolor="white" \
+            -Nfillcolor="none" -Ncolor="white" -Nfontcolor="white" \
+            -Efillcolor="none" -Ecolor="white" -Efontcolor="white" > ''${SVG_NAME}_dark.svg
+      done;
+    '';
 
     "blog:build" = {
       exec = "yarn run build";
